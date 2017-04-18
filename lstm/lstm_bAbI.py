7ce170dba9f3ead3af1afd4994ff99d3a040d3f1
Gabriel Jiménez <gabriel.jimenez102@alu.ulpgc.es>
2017-04-18 07:51:38 +0100

    Se añaden resultados

--- lstm/lstm_bAbI.py
+++ lstm/lstm_bAbI.py
@@ -14,7 +14,8 @@ import time
 
 start_time = time.time()
 
-tf.flags.DEFINE_float("learning_rate", 0.001, "Learning rate for Adam Optimizer.")
+tf.flags.DEFINE_boolean("L2", True, "Use L2 regularization or not")
+tf.flags.DEFINE_float("learning_rate", 1e-4, "Learning rate for Adam Optimizer.")
 tf.flags.DEFINE_float("epsilon", 1e-8, "Epsilon value for Adam Optimizer.")
 tf.flags.DEFINE_float("dropout", 0.75, "Dropout")
 # tf.flags.DEFINE_float("max_grad_norm", 40.0, "Clip gradients to this norm.")
@@ -23,13 +24,14 @@ tf.flags.DEFINE_integer("batch_size", 13, "Batch size for training.")
 # tf.flags.DEFINE_integer("epochs", 200, "Number of epochs to train for.")
 tf.flags.DEFINE_integer("task_id", 1, "bAbI task id, 1 <= id <= 20")
 # tf.flags.DEFINE_integer("random_state", None, "Random state.")
-tf.flags.DEFINE_integer("num_layers", 2, "Number of layers.")
-tf.flags.DEFINE_integer("num_hidden", 150, "Number of hidden units.")
+tf.flags.DEFINE_integer("num_layers", 1, "Number of layers.")
+tf.flags.DEFINE_integer("num_hidden", 100, "Number of hidden units.")
 # tf.flags.DEFINE_integer("steps", 25, "Number of steps in the LSTM.")
 tf.flags.DEFINE_string("data_dir", "data/en-valid/", "Directory containing bAbI tasks")
 tf.flags.DEFINE_string("log_dir", "data/logs/", "Directory containing bAbI tasks")
 FLAGS = tf.flags.FLAGS
 
+tf.reset_default_graph()
 print("Started Task:", FLAGS.task_id)
 
 # task data
@@ -62,47 +64,51 @@ def inference(_input_data):
         w_o = tf.Variable(tf.random_normal([FLAGS.num_hidden, 20], stddev=0.1), name='w_o')
         b_o = tf.Variable(tf.zeros([1]), name='b_o')
 
-        cells = [tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.BasicLSTMCell(num_units=FLAGS.num_hidden,
-                                                                            forget_bias=0.2), FLAGS.dropout) for _ in
-                 range(FLAGS.num_layers)]
+        lstm_layers = [tf.contrib.rnn.BasicLSTMCell(num_units=FLAGS.num_hidden, forget_bias=0.2)
+                       for _ in range(FLAGS.num_layers)]
 
-        lstm_cell = tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
+        lstm_layers_with_dropout = [tf.contrib.rnn.DropoutWrapper(layer, FLAGS.dropout) for layer in
+                                    lstm_layers]
+
+        lstm_system = tf.contrib.rnn.MultiRNNCell(lstm_layers_with_dropout, state_is_tuple=True)
 
         outputs = []
-        state = lstm_cell.zero_state(FLAGS.batch_size, tf.float32)
+        state = lstm_system.zero_state(FLAGS.batch_size, tf.float32)
         inp = _input_data[:, 0, :]
-        (cell_output, state) = lstm_cell(inp, state)
+        (cell_output, state) = lstm_system(inp, state)
         outputs.append(tf.nn.sigmoid(tf.matmul(cell_output, w_o) + b_o))
         tf.get_variable_scope().reuse_variables()
         for time_step in range(1, num_steps):
             inp = _input_data[:, time_step, :]
-            (cell_output, state) = lstm_cell(inp, state)
+            (cell_output, state) = lstm_system(inp, state)
             outputs.append(tf.nn.sigmoid(tf.matmul(cell_output, w_o) + b_o))
 
         result = tf.transpose(tf.squeeze(outputs), perm=[1, 0, 2])
         return result
 
-
 # Training ------------------------------------------------
-
 predicted = inference(input_data)
+if FLAGS.L2:
+    regularization_cost = tf.reduce_sum([tf.nn.l2_loss(v) for v in tf.trainable_variables()
+                                         if "w_o" in v.name or "weights" in v.name])
+    cross_entropy = tf.reduce_sum(tf.square(input_label - predicted[:, 62])) + regularization_cost
+else:
+    cross_entropy = tf.reduce_sum(tf.square(input_label - predicted[:, 62]))
 
-regularization_cost = tf.reduce_sum([tf.nn.l2_loss(v) for v in tf.trainable_variables() if v.name in (
-    'RNN/w_o:0',
-    'RNN/multi_rnn_cell/cell_0/basic_lstm_cell/weights:0',
-    'RNN/multi_rnn_cell/cell_1/basic_lstm_cell/weights:0')])
-
-cross_entropy = tf.reduce_sum(tf.square(input_label - predicted[:, 62])) + regularization_cost
 # train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
-train_step = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, epsilon=FLAGS.epsilon).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, epsilon=FLAGS.epsilon).minimize(
+    cross_entropy)
 accuracy = tf.reduce_sum(tf.abs(tf.round(predicted[:, 62]) - input_label))
+tf.summary.scalar("accuracy", accuracy)
+tf.summary.scalar("cross_entropy", cross_entropy)
 merged = tf.summary.merge_all()
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
+# writer = tf.summary.FileWriter(FLAGS.log_dir)
+# writer.add_graph(sess.graph)
 
 res = sess.run(cross_entropy, feed_dict={input_data: S[0:13], input_label: A[0:13]})
 
@@ -114,17 +120,21 @@ batches = zip(range(0, len(S[0]), FLAGS.batch_size), range(FLAGS.batch_size, len
 batches = [(start, end) for start, end in batches]
 
 i = 0
-for j in range(1000):
+for j in range(3000):
     for start, end in batches:
         sess.run(train_step, feed_dict={input_data: S[start:end], input_label: A[start:end]})
         i += 1
         if i % 20 == 0:
-            print("iteration: ", i, "ce: ", sess.run(cross_entropy,
-                                                     feed_dict={input_data: S[start:end], input_label: A[start:end]}))
+            [ce, summ] = sess.run([cross_entropy, merged],
+                                  feed_dict={input_data: S[start:end], input_label: A[start:end]})
+            # writer.add_summary(summ, i)
+            print("iteration: ", i, "ce: ", ce)
+            # if i % 500 == 0:
+            #     tf.train.Saver().save(sess, FLAGS.log_dir + "model.ckpt", i)
 
-tf.train.Saver().save(sess, FLAGS.log_dir + "model.ckpt", i)
 # Test ----------------------------------------------------
-test_batches = zip(range(0, len(Ste[0]), FLAGS.batch_size), range(FLAGS.batch_size, len(Ste[0]), FLAGS.batch_size))
+test_batches = zip(range(0, len(Ste[0]), FLAGS.batch_size),
+                   range(FLAGS.batch_size, len(Ste[0]), FLAGS.batch_size))
 test_batches = [(start, end) for start, end in test_batches]
 for start, end in test_batches:
     ce = sess.run(cross_entropy, feed_dict={input_data: Ste[start:end], input_label: Ate[start:end]})
