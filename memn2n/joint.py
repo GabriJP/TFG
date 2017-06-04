6939d00f4730c91e9fc003a3160b842a118aaa7d
Gabriel Jiménez <gabriel.jimenez102@alu.ulpgc.es>
2017-06-04 19:35:32 +0100

    Código de normal en N2N

--- memn2n/joint.py
+++ memn2n/joint.py
@@ -11,7 +11,6 @@ from six.moves import range, reduce
 
 import tensorflow as tf
 import numpy as np
-import csv
 
 tf.flags.DEFINE_float("learning_rate", 0.01, "Learning rate for SGD.")
 tf.flags.DEFINE_float("anneal_rate", 25, "Number of epochs between halving the learnign rate.")
@@ -28,128 +27,106 @@ tf.flags.DEFINE_integer("random_state", None, "Random state.")
 tf.flags.DEFINE_string("data_dir", "../familytask", "Directory containing bAbI tasks")
 FLAGS = tf.flags.FLAGS
 
-with open('deep.txt', 'w') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    for data_dir in ("data/tasks_1-20_v1-2/en-10k", "../familytask"):
-        for task_id in (3,):
-            for hops in range(6, 10):
-                tf.reset_default_graph()
-                print("Started Task:", task_id)
+print("Started Task:", FLAGS.task_id)
 
-                # task data
-                train, test = load_task(data_dir, task_id)
-                data = train + test
+# task data
+train, test = load_task(FLAGS.data_dir, FLAGS.task_id)
+data = train + test
 
-                vocab = sorted(reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q + a) for s, q, a in data)))
-                word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+vocab = sorted(reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q + a) for s, q, a in data)))
+word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
 
-                # noinspection PyRedeclaration
-                max_story_size = max(map(len, (s for s, _, _ in data)))
-                # noinspection PyRedeclaration
-                mean_story_size = int(np.mean([len(s) for s, _, _ in data]).round())
-                # noinspection PyRedeclaration
-                sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in data)))
-                # noinspection PyRedeclaration
-                query_size = max(map(len, (q for _, q, _ in data)))
-                memory_size = min(FLAGS.memory_size, max_story_size)
+# noinspection PyRedeclaration
+max_story_size = max(map(len, (s for s, _, _ in data)))
+# noinspection PyRedeclaration
+mean_story_size = int(np.mean([len(s) for s, _, _ in data]).round())
+# noinspection PyRedeclaration
+sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in data)))
+# noinspection PyRedeclaration
+query_size = max(map(len, (q for _, q, _ in data)))
+memory_size = min(FLAGS.memory_size, max_story_size)
 
-                # Add time words/indexes
-                for i in range(memory_size):
-                    word_idx['time{}'.format(i + 1)] = 'time{}'.format(i + 1)
+# Add time words/indexes
+for i in range(memory_size):
+    word_idx['time{}'.format(i + 1)] = 'time{}'.format(i + 1)
 
-                vocab_size = len(word_idx) + 1  # +1 for nil word
-                sentence_size = max(query_size, sentence_size)  # for the position
-                sentence_size += 1  # +1 for time words
+vocab_size = len(word_idx) + 1  # +1 for nil word
+sentence_size = max(query_size, sentence_size)  # for the position
+sentence_size += 1  # +1 for time words
 
-                print("Longest sentence length", sentence_size)
-                print("Longest story length", max_story_size)
-                print("Average story length", mean_story_size)
+print("Longest sentence length", sentence_size)
+print("Longest story length", max_story_size)
+print("Average story length", mean_story_size)
 
-                # train/validation/test sets
-                S, Q, A = vectorize_data(train, word_idx, sentence_size, memory_size)
-                trainS, valS, trainQ, valQ, trainA, valA = model_selection.train_test_split(S, Q, A, test_size=.1,
-                                                                                            random_state=FLAGS.random_state)
-                testS, testQ, testA = vectorize_data(test, word_idx, sentence_size, memory_size)
+# train/validation/test sets
+S, Q, A = vectorize_data(train, word_idx, sentence_size, memory_size)
+trainS, valS, trainQ, valQ, trainA, valA = model_selection.train_test_split(S, Q, A, test_size=.1,
+                                                                            random_state=FLAGS.random_state)
+testS, testQ, testA = vectorize_data(test, word_idx, sentence_size, memory_size)
 
-                print(testS[0])
+print(testS[0])
 
-                print("Training set shape", trainS.shape)
+print("Training set shape", trainS.shape)
 
-                # params
-                n_train = trainS.shape[0]
-                n_test = testS.shape[0]
-                n_val = valS.shape[0]
+# params
+n_train = trainS.shape[0]
+n_test = testS.shape[0]
+n_val = valS.shape[0]
 
-                print("Training Size", n_train)
-                print("Validation Size", n_val)
-                print("Testing Size", n_test)
+print("Training Size", n_train)
+print("Validation Size", n_val)
+print("Testing Size", n_test)
 
-                train_labels = np.argmax(trainA, axis=1)
-                test_labels = np.argmax(testA, axis=1)
-                val_labels = np.argmax(valA, axis=1)
+train_labels = np.argmax(trainA, axis=1)
+test_labels = np.argmax(testA, axis=1)
+val_labels = np.argmax(valA, axis=1)
 
-                tf.set_random_seed(FLAGS.random_state)
-                batch_size = FLAGS.batch_size
+tf.set_random_seed(FLAGS.random_state)
+batch_size = FLAGS.batch_size
 
-                batches = zip(range(0, n_train - batch_size, batch_size), range(batch_size, n_train, batch_size))
-                batches = [(start, end) for start, end in batches]
+batches = zip(range(0, n_train - batch_size, batch_size), range(batch_size, n_train, batch_size))
+batches = [(start, end) for start, end in batches]
 
-                with tf.Session() as sess:
-                    csv_writer.writerows((
-                        ("hops", "task_id", "data_dir", "Training Size", "Validation Size", "Testing Size"),
-                        (hops, task_id, data_dir, n_train, n_val, n_test),
-                        (),
-                        ("Epoch", "Total cost", "Training accuracy", "Validation Accuracy")
-                    ))
-                    csv_file.flush()
-                    model = MemN2N(batch_size, vocab_size, sentence_size, memory_size, FLAGS.embedding_size, session=sess,
-                                   hops=hops, max_grad_norm=FLAGS.max_grad_norm)
-                    for t in range(1, FLAGS.epochs + 1):
-                        # Stepped learning rate
-                        if t - 1 <= FLAGS.anneal_stop_epoch:
-                            anneal = 2.0 ** ((t - 1) // FLAGS.anneal_rate)
-                        else:
-                            anneal = 2.0 ** (FLAGS.anneal_stop_epoch // FLAGS.anneal_rate)
-                        lr = FLAGS.learning_rate / anneal
+with tf.Session() as sess:
+    model = MemN2N(batch_size, vocab_size, sentence_size, memory_size, FLAGS.embedding_size, session=sess,
+                   hops=FLAGS.hops, max_grad_norm=FLAGS.max_grad_norm)
+    for t in range(1, FLAGS.epochs + 1):
+        # Stepped learning rate
+        if t - 1 <= FLAGS.anneal_stop_epoch:
+            anneal = 2.0 ** ((t - 1) // FLAGS.anneal_rate)
+        else:
+            anneal = 2.0 ** (FLAGS.anneal_stop_epoch // FLAGS.anneal_rate)
+        lr = FLAGS.learning_rate / anneal
 
-                        np.random.shuffle(batches)
-                        total_cost = 0.0
-                        for start, end in batches:
-                            s = trainS[start:end]
-                            q = trainQ[start:end]
-                            a = trainA[start:end]
-                            cost_t = model.batch_fit(s, q, a, lr)
-                            total_cost += cost_t
+        np.random.shuffle(batches)
+        total_cost = 0.0
+        for start, end in batches:
+            s = trainS[start:end]
+            q = trainQ[start:end]
+            a = trainA[start:end]
+            cost_t = model.batch_fit(s, q, a, lr)
+            total_cost += cost_t
 
-                        if t % FLAGS.evaluation_interval == 0:
-                            train_preds = []
-                            for start in range(0, n_train, batch_size):
-                                end = start + batch_size
-                                s = trainS[start:end]
-                                q = trainQ[start:end]
-                                pred = model.predict(s, q)
-                                train_preds += list(pred)
+        if t % FLAGS.evaluation_interval == 0:
+            train_preds = []
+            for start in range(0, n_train, batch_size):
+                end = start + batch_size
+                s = trainS[start:end]
+                q = trainQ[start:end]
+                pred = model.predict(s, q)
+                train_preds += list(pred)
 
-                            val_preds = model.predict(valS, valQ)
-                            train_acc = metrics.accuracy_score(np.array(train_preds), train_labels)
-                            val_acc = metrics.accuracy_score(val_preds, val_labels)
+            val_preds = model.predict(valS, valQ)
+            train_acc = metrics.accuracy_score(np.array(train_preds), train_labels)
+            val_acc = metrics.accuracy_score(val_preds, val_labels)
 
-                            print('-----------------------')
-                            print('Epoch', t)
-                            print('Total Cost:', total_cost)
-                            print('Training Accuracy:', train_acc)
-                            print('Validation Accuracy:', val_acc)
-                            print('-----------------------')
-                            csv_writer.writerow((t, total_cost, train_acc, val_acc))
-                            csv_file.flush()
+            print('-----------------------')
+            print('Epoch', t)
+            print('Total Cost:', total_cost)
+            print('Training Accuracy:', train_acc)
+            print('Validation Accuracy:', val_acc)
+            print('-----------------------')
 
-                    test_preds = model.predict(testS, testQ)
-                    test_acc = metrics.accuracy_score(test_preds, test_labels)
-                    print("Testing Accuracy:", test_acc)
-                    csv_writer.writerows((
-                        (),
-                        ("Testing Accuracy:", test_acc),
-                        (),
-                        (),
-                        (),
-                    ))
+    test_preds = model.predict(testS, testQ)
+    test_acc = metrics.accuracy_score(test_preds, test_labels)
+    print("Testing Accuracy:", test_acc)
