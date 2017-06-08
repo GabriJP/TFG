4bdfa77c35e3bffb19d6252f74396af03b7ecd62
Gabriel Jiménez <gabriel.jimenez102@alu.ulpgc.es>
2017-06-08 00:33:28 +0000

    Second tests

--- dnc/script.sh
+++ dnc/script.sh
@@ -7,7 +7,10 @@ for task in ${input[@]}
 do
 	for iter in ${iters[@]}
 	do
-#		python3 train.py --input ${task} --output "${task}_${iter}"  --iterations ${iter}
+		date
+		python3 train.py --input ${task} --output "${task}_${iter}"  --iterations ${iter}
+		date
 		python3 test.py "${task}_${iter}" ${task} ${iter} > checkpoints/${task}_${iter}/test_results.txt
+		date
 	done
 done
